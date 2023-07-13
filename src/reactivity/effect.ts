acb41872015f34038bdfe0753cd57c699be7d903
Simonduya <duyalala@outlook.com>
2023-07-13 13:47:45 +0800

    Resolve(reactivity): feat effect.stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,11 +1,17 @@
 class ReactiveEffect {
     private _fn: any;
+    deps = [];
     constructor(fn, public scheduler?: any) {
         this._fn = fn;
     }
     run() {
         activeEffect = this;
         return this._fn();
+    }
+    stop() {
+        this.deps.forEach((dep: any) => {
+            dep.delete(this);
+        })
     }
 }
 
@@ -22,6 +28,7 @@ export function track(target, key) {
         depsMap.set(key, dep);
     }
     dep.add(activeEffect);
+    activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
@@ -41,5 +48,11 @@ export function effect(fn, options: any = {}) {
     const scheduler = options.scheduler;
     const _effect = new ReactiveEffect(fn, scheduler);
     _effect.run();
-    return _effect.run.bind(_effect);
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+}
+
+export function stop(runner) {
+    runner.effect.stop();
 }
