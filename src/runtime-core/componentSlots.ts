c7c354936fe5d808b303bce38699e39e1ecc9560
Simonduya <duyalala@outlook.com>
2023-08-09 22:10:12 +0800

    实现组件slots功能优化

--- src/runtime-core/componentSlots.ts
+++ src/runtime-core/componentSlots.ts
@@ -1,8 +1,13 @@
+import { ShapeFlags } from "./ShapeFlags";
+
 export function initSlots(instance, children) {
   // instance.slots = Array.isArray(children) ? children : [children];
   // console.log("instanceSlots", instance.slots);
-
-  normalizeObjectSlots(children, instance.slots);
+  const { vnode } = instance;
+  if (vnode.shapeFlag & ShapeFlags.SLOT_CHILDREN) {
+    normalizeObjectSlots(children, instance.slots);
+  }
+  
 }
 
 function normalizeObjectSlots(children: any, slots: any) {
