e6de783069bb612c1e3cc5e077569346b5290b09
Simonduya <duyalala@outlook.com>
2023-08-17 14:49:54 +0800

    Resolve(runtime-core): feat ArrayToArray 对比中间部分

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -82,10 +82,33 @@ import { h, ref } from "../../lib/mini-vue.esm.js";
 // a, b, (e, c), f, g
 // D节点在新的里面没有, 需要删除调
 // C节点的props发生了变化
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C", id: "c-prev" }, "C"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "C", id: "c-next" }, "C"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+
+// 5.1.1
+// a, b, (c,e, d), f, g
+// a, b, (e, c), f, g
+// 中间部分， 老的比新的多， 那么多出来的直接就可以删除
+// C节点的props发生了变化
 const prevChildren = [
   h("p", { key: "A" }, "A"),
   h("p", { key: "B" }, "B"),
   h("p", { key: "C", id: "c-prev" }, "C"),
+  h("p", { key: "E" }, "E"),
   h("p", { key: "D" }, "D"),
   h("p", { key: "F" }, "F"),
   h("p", { key: "G" }, "G"),
